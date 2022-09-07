f06f81a94c332ce3c0c5aed4c659b974f95f55af
Pedro Leite <pedroaug.fl@gmail.com>
2022-09-07 13:02:32 -0300

    enterRaffle fct working

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -4,21 +4,33 @@ pragma solidity ^0.8.9;
 
 // Raffle
 
+// Enter the lottery (paying some amount)
+// Pick a random winner (verifiably random)
+// Winner to be selected every X minutes -> completely automate
+// Chainlink Oracle -> Randomness, Automated Execution
+
+error Raffle__NotEnoughETHEntered();
+
 contract Raffle {
     uint256 private immutable i_entranceFee;
+    address payable[] private s_players;
 
     constructor(uint256 entranceFee) {
         i_entranceFee = entranceFee;
     }
 
-    // Enter the lottery (paying some amount)
-    function enterRaffle() {}
+    function enterRaffle() public payable {
+        if (msg.value < i_entranceFee) {
+            revert Raffle__NotEnoughETHEntered();
+        }
+        s_players.push(payable(msg.sender));
+    }
 
     function getEntranceFee() public view returns (uint256) {
         return i_entranceFee;
     }
-}
 
-// Pick a random winner (verifiably random)
-// Winner to be selected every X minutes -> completely automate
-// Chainlink Oracle -> Randomness, Automated Execution
+    function getPlayers(uint256 index) public view returns (address) {
+        return s_players[index];
+    }
+}
