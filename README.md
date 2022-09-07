6cfd4d94b86506be4092881c993afadbb21f2749
Pedro Leite <pedroaug.fl@gmail.com>
2022-09-07 19:44:55 -0300

    added event

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # hardhat-smartcontract-lottery-fcc
 
-[https://www.youtube.com/watch?v=gyMwXuJrbJQ&t=57900s&ab_channel=freeCodeCamp.org&t=14h01m21s](https://www.youtube.com/watch?v=gyMwXuJrbJQ&t=57900s&ab_channel=freeCodeCamp.org&t=14h01m21s)
+[https://www.youtube.com/watch?v=gyMwXuJrbJQ&t=57900s&ab_channel=freeCodeCamp.org&t=14h09m48s](https://www.youtube.com/watch?v=gyMwXuJrbJQ&t=57900s&ab_channel=freeCodeCamp.org&t=14h09m48s)
--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -15,6 +15,8 @@ contract Raffle {
     uint256 private immutable i_entranceFee;
     address payable[] private s_players;
 
+    event RaffleEnter(address indexed player);
+
     constructor(uint256 entranceFee) {
         i_entranceFee = entranceFee;
     }
@@ -24,6 +26,7 @@ contract Raffle {
             revert Raffle__NotEnoughETHEntered();
         }
         s_players.push(payable(msg.sender));
+        emit RaffleEnter(msg.sender);
     }
 
     function getEntranceFee() public view returns (uint256) {
