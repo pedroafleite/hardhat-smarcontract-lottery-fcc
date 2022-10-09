7fd652fe239a02f0e674a54bf19aa4aa2080b846
Pedro Leite <pedroaug.fl@gmail.com>
2022-10-09 09:17:54 -0300

    successful deployment

--- deploy/01-deploy-raffle.js
+++ deploy/01-deploy-raffle.js
@@ -1,11 +1,12 @@
 const { network, ethers } = require("hardhat")
 const { developmentChains, networkConfig } = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
 
-const VRF_SUB_FUND_AMOUNT = ethers.utils.parseEther("30")
+const VRF_SUB_FUND_AMOUNT = ethers.utils.parseEther("1")
 
 module.exports = async function ({ getNamedAccounts, deployments }) {
     const { deploy, log } = deployments
-    const { deployer } = await getNamedAccounts
+    const { deployer } = await getNamedAccounts()
     const chainId = network.config.chainId
     let vrfCoordinatorV2Address
 
@@ -42,4 +43,12 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
         log: true,
         waitConfirmations: network.config.blockConfirmations || 1,
     })
+
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        log("Verifying...")
+        await verify(raffle.address, args)
+    }
+    log("-------------------------------------------------")
 }
+
+module.exports.tags = ["all", "raffle"]
